55a726e99660b2b58b5de821171df568132fc292
Fatih Koçer <fatihkocerc@gmail.com>
2022-09-25 13:35:28 +0300

    include necessary header files for icmp.c

--- src/icmp.c
+++ src/icmp.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 #include "sockf.h"
