bf976cdf99c267184b9d96ce036ee6eaa877aff5
Fatih Koçer <fatihkocerc@gmail.com>
2023-01-03 19:36:54 +0300

    fix argument name in inject_tcp()

--- src/tcp.h
+++ src/tcp.h
@@ -4,6 +4,6 @@
 void set_tcp(char *buffer, char *payload, size_t payload_size,
 		unsigned short src, unsigned short dst,
 		unsigned char flag, unsigned int seq, unsigned int ack);
-void inject_tcp(int arc, char *argv[]);
+void inject_tcp(int argc, char *argv[]);
 
 #endif
